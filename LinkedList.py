0f07503a9153ec5c4f8078b469cd94d3367d4e8e
boychenkoaa <boychenkoaa.r@gmail.com>
2023-10-28 00:48:54 +0300

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,119 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+    
+    def __str__(self):
+        ans = ""
+        node = self.head
+        while node != None:
+            ans += str(node.value) + " "
+            node = node.next
+        return ans
+        
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value, end=' ')
+            node = node.next
+        print('')
+        
+        
+    def find(self, val):
+        node = self.head
+        while node != None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        ans = []
+        node = self.head
+        while node != None:
+            if node.value == val:
+                ans.append(node)
+            node = node.next
+        return ans
+
+    def delete(self, val, all=False):
+        if self.isEmpty:
+            return
+                    
+        prehead = Node(None)
+        prehead.next = self.head
+        item = prehead
+        while item.next != None:
+            if item.next.value == val:
+                item.next = item.next.next
+                if not all:
+                    break
+            else:
+                item = item.next
+        
+        self.head = prehead.next
+        prehead = None # !!!
+        self.tail = item
+                
+    def clean(self):
+        self.head = None
+        self.tail = None
+        
+    def len(self):
+        ans = 0
+        node = self.head
+        while node != None:
+            ans += 1
+            node = node.next        
+        return ans # здесь будет ваш код
+
+    def insert(self, afterNode, newNode):
+        if self.isEmpty:
+            self.head = newNode
+            self.tail = newNode
+        # to head
+        elif afterNode is None:
+            newNode.next = self.head
+            self.head = newNode
+        # to tail
+        elif afterNode.next is None:
+            afterNode.next = newNode
+            self.tail = newNode
+            newNode.next = None
+        else:
+            newNode.next = afterNode.next
+            afterNode.next = newNode
+    
+    @property        
+    def isEmpty(self):
+        return self.head is None
+            
+li = LinkedList()
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.add_in_tail(Node(4))
+li.print_all_nodes()
+li.delete(4, all=True)
+li.print_all_nodes()
+print(li.head, li.tail)
+li.insert(li.head, Node(5))
+li.print_all_nodes()
+print(li.find_all(4))
+print(li.len())
