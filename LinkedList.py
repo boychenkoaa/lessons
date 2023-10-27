b53e309e1d470edc0b06bcafb16697c2641989c8
boychenkoaa <boychenkoaa.r@gmail.com>
2023-10-28 01:27:24 +0300

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -100,3 +100,16 @@ class LinkedList:
     @property        
     def isEmpty(self):
         return self.head is None
+
+    
+def list_sum(list1: LinkedList, list2: LinkedList):
+    if list1.len() == list2.len():
+        node1 = list1.head
+        node2 = list2.head
+        ans = LinkedList()
+        while node1 != None:
+            ans.add_in_tail(Node(node1.value + node2.value))
+            node1 = node1.next
+            node2 = node2.next
+        return ans
+    return None
