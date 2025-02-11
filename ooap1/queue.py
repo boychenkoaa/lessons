c3b8142eec47fa502260d95f35da25925e2c1c80
boychenkoaa <boychenkoaa.r@gmail.com>
2025-02-11 21:33:22 +0300

    Update queue.py

--- ooap1/queue.py
+++ ooap1/queue.py
@@ -1,8 +1,8 @@
 from typing import TypeVar, Generic
 from parent_list import TwoWayList, Status
 
+# АТД Queue - очередь
 T = TypeVar("T")
-
 class Queue(Generic[T]):
     def __init__(self):
         self._q_status = Status.NIL
@@ -10,23 +10,29 @@ class Queue(Generic[T]):
         self._li = TwoWayList[T]
     
     # запросы без предусловий
+
+    # размер очереди
     @property
     def size(self):
         return self._li.size
     
+    # статус последнего запроса с предусловием
     @property
     def q_status(self):
         return self._q_status
-       
+
+    # статус последней команды с предусловием
     @property 
     def c_status(self):
         return self._c_status
-    
+
+    # пустая ли очередь
     @property     
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self._li.is_value
     
-    # запрос, предусловие: не пуст
+    # запрос на первый элемент очереди
+    # предусловие: очередь не пуста
     @property    
     def first(self) -> T:
         self._li.head
@@ -38,8 +44,9 @@ class Queue(Generic[T]):
             self._q_status = Status.ERR
             return None        
         return ans
-    
-    # запрос, предусловие: не пуст    
+
+    # запрос на последний элемент очереди
+    # предусловие: не пуст    
     @property
     def last(self) -> T:
         self._li.tail
@@ -51,8 +58,10 @@ class Queue(Generic[T]):
             self._q_status = Status.ERR
             return None        
         return ans
-        
+
+    # удалить элемент из хвоста очереди
     # команда, предусловие: не пуст
+    # постусловие -- удален 1 элемент
     def pop(self):
         self._li.head
         if self._li.head_status == Status.ERR:
@@ -60,8 +69,10 @@ class Queue(Generic[T]):
             return
         self._li.remove
         self._c_status = self._li.remove_status
-        
-    # команда без предусловий
+
+    # команда 
+    # положить элемент в голову очереди
+    # постусловие -- добавлен 1 элемент
     def push(self, new_value: T):
         if self.is_empty:
             self._li.add_to_empty(new_value)
@@ -71,6 +82,7 @@ class Queue(Generic[T]):
         self._li.add_tail(new_value)
         self._c_status = self._li.add_tail_status
         
-    # команда без условий
+    # команда 
+    # постусловие -- очередь пуста
     def clear(self):
         self._li.clear()
