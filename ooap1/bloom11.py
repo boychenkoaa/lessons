c2d789c568e83804cdd45a0c2dd0e6025943b5be
boychenkoaa <boychenkoaa.r@gmail.com>
2025-02-11 22:04:12 +0300

    Update bloom11.py

--- ooap1/bloom11.py
+++ ooap1/bloom11.py
@@ -13,6 +13,9 @@ class Status(Enum):
 # фильтр Блума для строковых значений
 
 class SimpleBloomFilter:
+    # констуктор пустой
+    # размер фильтра берется по умолчанию
+    
     def __init__(self):
         self._size = BLOOM_FILTER_SIZE
         self._counts = [0] * self._size
@@ -23,48 +26,58 @@ class SimpleBloomFilter:
     @property
     def remove_status(self) -> Status:
         return self._remove_status
-    
+
+    # запрос "внутренний" но вынужденно сделанный публичным
     # массив значений по индексам
     @property
     def counts(self) -> list[int]:
         return deepcopy(self._counts)
 
+    # запрос -- размер фильтра
     @property
     def size(self) -> int:
         return self._size
     
-    # хеш
+    # внутренний запрос
+    # хеш функция
     def _hash17(self, s: str) -> int:
         # 17
         ans = sum((17 * ans + ord(c)) % self._size for c in s)
         return ans        
 
-    # хеш
+    # внутренний запрос
+    # хеш функция
     def _hash223(self, s: str) -> int:
         # 223 
         ans = sum((17 * ans + ord(c)) % self._size for c in s)
         return ans        
-
-    # запрос - кортеж из хешей-индексов, где нужно увеличить на 1
+    
+    # внутренний запрос
+    # кортеж из хешей - будущих индексов в _counts
     def _hash_tuple(self, s: str) -> int:
         return (self._hash17(s), self._hash223(s))
     
+    # запрос
     # проверка значения на вхождение
     def is_value(self, s: str) -> bool:
         return all(self.counts[i] > 0 for i in self._hash_tuple(s))
              
-    # далее идут команды
+    # команды
+
     # добавление элемента в фильтр
+    # постусловие -- добавлен элемент
     def add(self, s: str):
         for i in self._hash_tuple(s):
             self.counts[i] += 1        
 
-    # слияние с другим фильтро
+    # слияние с другим фильтром
+    # постусловие -- новый фильтр есть сумма двух слагаемых
     def merge_with(self, other: SimpleBloomFilter):
         self._counts = [x + y for x, y in zip (self.counts, other.counts)]
     
-    # предусловие: удаляемый есть
     # удаление элемента
+    # предусловие: удаляемый есть
+    # постусловия нет (ложно-положительные срабатывания возможны)
     def remove(self, s: str):
         if not self.is_value(s):
             self._remove_status = Status.ERR
@@ -74,6 +87,7 @@ class SimpleBloomFilter:
             self.counts[i] -= 1
     
     # очистка
+    # постусловие -- фильтр пуст
     def clear(self):
         self._counts = [0] * self._size
         self._remove_status = Status.NIL
