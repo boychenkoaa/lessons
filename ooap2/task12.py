a986b3c481af53f7ec944c1c1824ab6c241f3a5a
boychenkoaa <boychenkoaa.r@gmail.com>
2025-06-15 14:47:33 +0300

    Create task12.py

---
+++ ooap2/task12.py
@@ -0,0 +1,43 @@
+'''
+Задание 12. Попытка присваивания.
+У меня получилось, что Any не нужндается в переопределении попытки присваивания, и ее можно сделать "финальной" в General
+"конструктора копирования" в Python нет
+'''
+
+class General:
+    ...
+  
+    @final
+    @classmethod
+    def try_set(cls, other) -> _T:
+        if isinstance(other, cls):
+            return other
+
+        return MyNone()
+
+
+class Any(General):
+    ...
+
+class MyNone(Any):
+    ...
+
+# тестируем
+if __name__ == '__main__':
+    # при попытке присваивания копируется ссылка либо возвращается None
+    g = General()
+    a = Any()
+    g = g.try_set(a)
+    assert a == g
+    assert a is g
+
+    # неприводимые типы
+    g = General()
+    a = Any()
+    a = a.try_set(g)
+    assert isinstance(a, MyNone)
+
+    # еще одни неприводимые типы
+    a = Any()
+    a = a.try_set(1)
+    assert isinstance(a, MyNone)
