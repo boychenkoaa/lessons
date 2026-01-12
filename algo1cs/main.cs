ded5e599620f958eefae01813807832846fa73b5
Alex <bojchenko_aa@spbstu.ru>
2026-01-12 17:58:26 +0300

    task1_begin
    хеллоу ворлд добавил

---
+++ algo1cs/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AlgorithmsDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
